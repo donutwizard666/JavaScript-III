cbbcff17f64e1ad83aea1e3454ca0622d6ec4038
donutwizard666 <thebeardedwarrior@icloud.com>
2019-09-18 08:47:59 -0700

    this js done

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,67 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Binding - When in the "world" (global scope), the value of THIS will be the console Object.
+
+* 2. Implicit Binding - whenever a function is called by a dot, the object to the left of the dot is assigned THIS. THIS points to the entire object. 
+
+* 3. New Binding - Whenever a constructor function is used THIS is referring to the specific instance of the object that is created and returned by the constructor function.
+
+* 4. Explicit Binding - You are overriding what the THIS keyword points to. Whenever the call or apply method is used, THIS is explicitly defined. 
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+function sayMyName(name) {
+    console.log(this);
+    return name;
+}
+sayMyName("Ryan");
 
 // Principle 2
 
-// code example for Implicit Binding
+const magicObj = {
+    greeting: 'Yo, what is up',
+    sayHello: function(name) {
+        console.log(`${this.greeting} my name is ${name}`);
+        console.log(this);
+    }
+};
+magicObj.sayHello('Albus Dumbledore');
 
 // Principle 3
 
-// code example for New Binding
+function wizardPeople(greeter) {
+    this.greeting = 'Hello! ';
+    this.greeter = greeter;
+    this.speak = function() {
+        console.log(this.greeting + this.greeter);
+        console.log(this);
+    };
+}
+const Albus = new wizardPeople('Hermione');
+const Hermione = new wizardPeople('Albus');
+
+Albus.speak();
+Hermione.speak();
 
 // Principle 4
 
-// code example for Explicit Binding
+function wizardPeople(greeter) {
+    this.greeting = 'Hello! ';
+    this.greeter = greeter;
+    this.speak = function() {
+        console.log(this.greeting + this.greeter);
+        console.log(this);
+    };
+}
+const Albus = new wizardPeople('Hermione');
+const Hermione = new wizardPeople('Albus');
+
+Albus.speak.call(Hermione);
+Hermione.speak.apply(Albus);
+
+Albus.speak();
+Hermione.speak();
